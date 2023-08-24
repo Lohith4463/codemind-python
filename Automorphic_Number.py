40bb3c07de1c4286ef8bdf21989155acd7f26cda
Lohith4463 <119839306+Lohith4463@users.noreply.github.com>
2023-08-24 14:29:00 +0530

    def automorphic(n):     s=n*n     sn=str(n)     ss=str(s)     if ss.endswith(sn):         return True     return False num=int(input()) if automorphic(num):     print("Automorphic Number") else:     print("Not an Automorphic Number")

---
+++ Automorphic_Number.py
@@ -0,0 +1,12 @@
+def automorphic(n):
+    s=n*n
+    sn=str(n)
+    ss=str(s)
+    if ss.endswith(sn):
+        return True
+    return False
+num=int(input())
+if automorphic(num):
+    print("Automorphic Number")
+else:
+    print("Not an Automorphic Number")
